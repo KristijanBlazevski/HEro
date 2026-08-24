95da2f38796ec80773917cb5bde6b24c1aebb5f1
Kristijan Blazhevski <kristijan.blazevski@yahoo.com>
2026-08-24 19:12:54 +0200

    Enemy movement

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Rigidbody2D rb;
+    private Transform player;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    
+    }
+
+    private void Start()
+    {
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+
+        
+
+        player = playerObject.transform;
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb == null || player == null)
+            return;
+
+        Vector2 direction = (player.position - transform.position).normalized;
+
+        rb.linearVelocity = direction * moveSpeed;
+    }
+}
